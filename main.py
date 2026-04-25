b6e19aefcee54f7c8860e3cbd568bb3e2bfe9911
Andrey Frolov <darmaley40@yandex.ru>
2026-04-25 08:10:18 +0300

    remove style scoped

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """main."""
 
+import re
 from pathlib import Path
 
 import nbformat
@@ -17,14 +18,14 @@ def convert_and_merge(notebook_paths: list[str], output_md: str) -> None:
 
         # Конвертируем в Markdown
         (body, resources) = markdown_exporter.from_notebook_node(notebook)
-
+        body_cleaned = re.sub(r"<style scoped>.*?</style>", "", body, flags=re.DOTALL)
         # # Добавляем заголовок с именем файла (опционально)
         # full_content.append(f"\n\n# {nb_path}\n\n")
-        full_content.append(body)
+        full_content.append(body_cleaned)
 
     # Сохраняем объединённый файл
     with Path(output_md).open("w", encoding="utf-8") as f:
-    # with open(output_md, "w", encoding="utf-8") as f:
+        # with open(output_md, "w", encoding="utf-8") as f:
         f.write("\n".join(full_content))
 
 
